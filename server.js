b38ad46ab50555503da5e4f0b7e979bb4b5e32e5
r4ul-pcc <174156265+r4ul-pcc@users.noreply.github.com>
2025-08-08 12:50:15 -0500

    2

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 80;
 
 app.get('/', (req, res) => {
     res.send('¡Hola desde Node.js!');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+app.listen(80, "0.0.0.0", () => console.log("Servidor en puerto 80"));
