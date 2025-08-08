e61ca894d54fc9f5d3c509ad854a282d090bcb46
r4ul-pcc <174156265+r4ul-pcc@users.noreply.github.com>
2025-08-08 12:47:25 -0500

    .

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = 80;
 
 app.get('/', (req, res) => {
     res.send('¡Hola desde Node.js!');
